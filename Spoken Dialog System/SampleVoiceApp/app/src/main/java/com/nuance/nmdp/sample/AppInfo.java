c7b879f9c1060598a0bdeca5ca8f0b14fdacea92
Andrey Kolchanov <mymacbook@Andreys-MacBook-Pro.local>
2015-04-03 01:17:09 +0300

    Renamed folder

--- Spoken Dialog System/SampleVoiceApp/app/src/main/java/com/nuance/nmdp/sample/AppInfo.java
+++ Spoken Dialog System/SampleVoiceApp/app/src/main/java/com/nuance/nmdp/sample/AppInfo.java
@@ -27,13 +27,13 @@ public class AppInfo
      */
 
     /* Please contact Nuance to receive the necessary connection and login parameters */
-    public static final String SpeechKitServer = ;
+    public static final String SpeechKitServer = "sandbox.nmdp.nuancemobility.net";
 
-    public static final int SpeechKitPort = ;
+    public static final int SpeechKitPort = 443;
 
     public static final boolean SpeechKitSsl = false;
 
-    public static final String SpeechKitAppId = ;
-    public static final byte[] SpeechKitApplicationKey = ;
+    public static final String SpeechKitAppId = "NMDPTRIAL_andrey462320150314162148";
+    public static final byte[] SpeechKitApplicationKey = {(byte)0xe2, (byte)0xab, (byte)0x4d, (byte)0xc6, (byte)0x05, (byte)0x27, (byte)0x2b, (byte)0x8e, (byte)0xf2, (byte)0x7a, (byte)0x06, (byte)0xfa, (byte)0x7a, (byte)0xd3, (byte)0x0f, (byte)0x40, (byte)0xf2, (byte)0x85, (byte)0xb0, (byte)0xca, (byte)0x0f, (byte)0x3a, (byte)0xdb, (byte)0xb2, (byte)0x4d, (byte)0x9b, (byte)0xf3, (byte)0x04, (byte)0x31, (byte)0x63, (byte)0x13, (byte)0x14, (byte)0x0e, (byte)0x60, (byte)0xda, (byte)0x13, (byte)0x87, (byte)0x05, (byte)0xa1, (byte)0x79, (byte)0x7f, (byte)0xa8, (byte)0x73, (byte)0xf9, (byte)0x7b, (byte)0x82, (byte)0x39, (byte)0x6e, (byte)0x07, (byte)0xe9, (byte)0x38, (byte)0x16, (byte)0x24, (byte)0x13, (byte)0xc8, (byte)0x71, (byte)0xae, (byte)0xbb, (byte)0x75, (byte)0xe0, (byte)0x1e, (byte)0x88, (byte)0xf4, (byte)0x2c};
 
 }
